c00878e2f5a606ef98995dce0b98b3faf9e25a89
yuanchaoxi <yuanchaoxi@huawei.com>
2016-10-26 09:21:15 +0800

    add stun

--- src/client/lz.rtc.peerManager.js
+++ src/client/lz.rtc.peerManager.js
@@ -7,13 +7,31 @@
 ;(function (rtc) {
     var iceServer = {"iceServers": [
             {"url": "stun:stun.l.google.com:19302"},
-            {"url": "stun:stunserver.org:3478"},
-            {"url": "stun:stun.xten.com:3478"},
-            {"url": "stun:stun.fwdnet.net:3478"},
-            {"url": "stun:stun.wirlab.net:3478"},
-            {"url": "stun:stun.iptel.org:3478"},
-            {"url": "stun:stun.ekiga.net:3478"},
-
+            {"url": "stun:stunserver.org"},
+            {"url": "stun:stun.xten.com"},
+            {"url": "stun:stun.fwdnet.net"},
+            {"url": "stun:stun.wirlab.net"},
+            {"url": "stun:stun.iptel.org"},
+            {"url": "stun:stun.ekiga.net"},
+            {url:'stun:stun01.sipphone.com'},
+            {url:'stun:stun.ekiga.net'},
+            {url:'stun:stun.fwdnet.net'},
+            {url:'stun:stun.ideasip.com'},
+            {url:'stun:stun.iptel.org'},
+            {url:'stun:stun.rixtelecom.se'},
+            {url:'stun:stun.schlund.de'},
+            {url:'stun:stun.l.google.com:19302'},
+            {url:'stun:stun1.l.google.com:19302'},
+            {url:'stun:stun2.l.google.com:19302'},
+            {url:'stun:stun3.l.google.com:19302'},
+            {url:'stun:stun4.l.google.com:19302'},
+            {url:'stun:stunserver.org'},
+            {url:'stun:stun.softjoys.com'},
+            {url:'stun:stun.voiparound.com'},
+            {url:'stun:stun.voipbuster.com'},
+            {url:'stun:stun.voipstunt.com'},
+            {url:'stun:stun.voxgratia.org'},
+            {url:'stun:stun.xten.com'},
         ]}
         , _channel
         , noop = function(){}
@@ -136,4 +154,3 @@
     }
 
 })(lz.rtc);
-
